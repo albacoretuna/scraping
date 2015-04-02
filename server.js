d8d41686059892f86c3d1fcb486656e2ff731dd1
Omid Hezaveh <ommmmid@gmail.com>
2015-04-02 23:19:23 +0300

    main scraping function added

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ var app = express();
 app.get('/scrape', function(req, res) {
 
     // web scraping in here! 
+    url = 'http://www.imdb.com/title/tt1229340/';
+
+    request(url, function(error, respone, html) {
+        if(!error) {
+            var $ = cheerio.load(html);
+
+            var title, release, rating;
+            var json = { title: "", release : "", rating: ""};
+        }
+    })
 })
 app.listen('8081')
 
