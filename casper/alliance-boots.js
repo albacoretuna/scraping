e9b27eff297b1e5aca62817478da4fc8453037d2
Omid Hezaveh <ommmmid@gmail.com>
2015-04-14 19:34:39 +0300

    Return one set of shops on page by postcode prefix
    Giving a single prefix, searches for that and returns all the pages for that prefix. It echoes all the shop info.

--- casper/alliance-boots.js
+++ casper/alliance-boots.js
@@ -5,10 +5,11 @@
 
 var baseUrl = 'https://m.boots.com/h5/storeLocator_hub?pageType=storeLocator&unCountry=uk',
     postcodePrefixes = ['AB10','AB11','AB12','AB13'],
+    shopTotalPerPage,
     fs = require('fs');
 
 var casper = require('casper').create({
-    verbose: true,
+    verbose: false,
     logLevel: 'debug',
     pageSettings: {
         loadImages:  false,
@@ -16,10 +17,25 @@ var casper = require('casper').create({
         }
 });
 
+function getShopInfo() {    
+    shopInfo = casper.evaluate(function() {
+        var shopTotalPerPage = map.entities.getLength();
+        var shopsOnPage = [];
+        for(var k = 0; k < shopTotalPerPage; k++) {
+          shopsOnPage.push(map.entities.get(k)); 
+        }
+
+        return shopsOnPage;
+    });
+    casper.echo(JSON.stringify(shopInfo));
+}
+
 //*[@id="postcode"]
 casper.start(baseUrl, function() {
     });
 casper.then(function() {
     this.fill('form#storLocator', { 'postcode' : 'B1'}, true);
 });
+casper.then(getShopInfo);
+
 casper.run();    
