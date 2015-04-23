b6df8e13f15670fee9b64a7d857d5fd1e02b191d
Omid Hezaveh <ommmmid@gmail.com>
2015-04-23 19:47:29 +0300

    Return null instead of shops!

--- casper/three.co.js
+++ casper/three.co.js
@@ -3,7 +3,12 @@
  */
 
 
-var baseUrl = "http://www.three.co.uk/storelocator/locate?lat=50.5073509&lng=-0.12775829999998223&radius=100";
+var i = 0;
+var prefCoords = [["AB10",57.131086,-2.122482],["AB11",57.13121,-2.082261]];
+
+// prefCoords[i][1] = lat and [i][2] = long
+var baseUrl = "http://www.three.co.uk/storelocator/locate?lat="+prefCoords[i][1]+"&lng="+prefCoords[i][2]+"&radius=300";
+
 
 
 var casper = require('casper').create({
@@ -14,3 +19,38 @@ var casper = require('casper').create({
         loadPlugins: false
         }
     });
+
+casper.start();
+
+casper.eachThen(prefCoords, function reqAndGrab() {
+    casper.open(baseUrl);
+    this.capture('screenshots/three.co.png');
+    var shopsOnPage = casper.evaluate(function(){ 
+    var storeNames = document.querySelectorAll('stores >store > name'); 
+    var storeLats = document.querySelectorAll('stores > store > lat'); 
+    var storeLongs = document.querySelectorAll('stores > store > lng'); 
+    storeNames = Array.prototype.map.call(storeNames, function(e) {
+        return e.textContent;
+    });
+    storeLats = Array.prototype.map.call(storeLats, function(e) {
+        return e.textContent;
+    });
+    storeLongs = Array.prototype.map.call(storeNames, function(e) {
+        return e.textContent;
+    });
+
+    var shopsPerPage = storeNames.length;
+    var shopsOnPage = [];
+    for(var ix = 0; ix < shopsPerPage; ix++){
+        shopsOnPage.push([storeLats[ix], storeLongs[ix],storeNames[ix]]);
+}
+        return shopsOnPage;
+    });
+    casper.echo(JSON.stringify(shopsOnPage)); 
+    
+    i++;
+});
+
+    
+
+casper.run();
