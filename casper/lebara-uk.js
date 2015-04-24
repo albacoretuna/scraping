894707efb82e85f16bd34c4121afd579ebd25723
Omid Hezaveh <ommmmid@gmail.com>
2015-04-24 20:05:31 +0300

    lebara iterate over all pages return nothing yet

--- casper/lebara-uk.js
+++ casper/lebara-uk.js
@@ -11,8 +11,7 @@ var i = 0,
 
 var prefCoords = [["AB10",57.131086,-2.122482],["AB10",57.131086,-2.122482],["E1",51.514997,-0.058707],["AB11",57.13121,-2.082261],["E2",51.529447,-0.060197]];
 
-var baseUrl = "http://www.lebara.co.uk/view/StoreLocatorComponentController?findStores=true&lat=53.8007554&lng=-1.5490773999999874&currentLocation=Leeds&distance=0&distanceUnit=Miles&offeringType=SIM&_offeringType=on&_offeringType=on&defaultSearchableLocs=STORE%2C+HOTSPOT%2C+MICROEVENT%2C+BRANDED_STORE&pageNumber=0";
-var baseUrlVar = "http://www.lebara.co.uk/view/StoreLocatorComponentController?findStores=true&lat=53.8007554&lng=-1.5490773999999874&currentLocation=Leeds&distance=0&distanceUnit=Miles&offeringType=SIM&_offeringType=on&_offeringType=on&defaultSearchableLocs=STORE%2C+HOTSPOT%2C+MICROEVENT%2C+BRANDED_STORE&pageNumber="+i;
+var baseUrl = "http://www.lebara.co.uk/view/StoreLocatorComponentController?findStores=true&lat=53.8007554&lng=-1.5490773999999874&currentLocation=Leeds&distance=0&distanceUnit=Miles&offeringType=SIM&_offeringType=on&_offeringType=on&defaultSearchableLocs=STORE%2C+HOTSPOT%2C+MICROEVENT%2C+BRANDED_STORE&pageNumber="+i;
 
 
 var casper = require('casper').create({
@@ -35,7 +34,7 @@ casper.thenOpen(baseUrl, function() {
 casper.then(function(){
     casper.echo("pageContent is now: "+pageContent);
     for(var i = 0; i < totalPages; i++) {
-        casper.thenOpen(baseUrlVar, function(){
+        casper.thenOpen(baseUrl+i, function(){
             
             });
 
