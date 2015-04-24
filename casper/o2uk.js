09b790634856da872a373dbc91537cb4b996c325
Omid Hezaveh <ommmmid@gmail.com>
2015-04-24 10:10:07 +0300

    Run onlyuniqe on results on each cycle o2uk

--- casper/o2uk.js
+++ casper/o2uk.js
@@ -18,6 +18,22 @@ var casper = require('casper').create({
         }
     });
 
+function onlyUnique(items) { 
+    for (var ix=0; ix<items.length; ix++) {
+    var listI = items[ix];
+    loopJ: for (var jx=0; jx<items.length; jx++) {
+        var listJ = items[jx];
+        if (listI === listJ) continue; //Ignore itself
+        for (var kx=listJ.length; kx>=0; kx--) {
+            if (listJ[kx] !== listI[kx]) continue loopJ;
+        }
+        // At this point, their values are equal.
+        items.splice(jx, 1);
+        }
+    }
+
+    return items;
+}
 
 
 casper.start();
@@ -40,6 +56,7 @@ casper.eachThen(prefCoords, function reqAndGrab() {
          }); 
 
     shopInfo = shopInfo.concat(shopsOnPage);
+    onlyUnique(shopInfo);
 });
 
 
