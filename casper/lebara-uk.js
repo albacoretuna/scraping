427600a0d31b636c16280a3473690024d7ce569b
Omid Hezaveh <ommmmid@gmail.com>
2015-04-24 20:41:42 +0300

    lebaraUK turn off offeringtype to grab all stores
    If you turn offeringtype = SIM you get smaller amount of stores, but like this it returns every damn shop that sells topups for this operator

--- casper/lebara-uk.js
+++ casper/lebara-uk.js
@@ -9,9 +9,8 @@ var i = 0,
     pageContent,
     totalPages;
 
-var prefCoords = [["AB10",57.131086,-2.122482],["AB10",57.131086,-2.122482],["E1",51.514997,-0.058707],["AB11",57.13121,-2.082261],["E2",51.529447,-0.060197]];
 
-var baseUrl = "http://www.lebara.co.uk/view/StoreLocatorComponentController?findStores=true&lat=53.8007554&lng=-1.5490773999999874&currentLocation=Leeds&distance=0&distanceUnit=Miles&offeringType=SIM&_offeringType=on&_offeringType=on&defaultSearchableLocs=STORE%2C+HOTSPOT%2C+MICROEVENT%2C+BRANDED_STORE&pageNumber="+i;
+var baseUrl = "http://www.lebara.co.uk/view/StoreLocatorComponentController?findStores=true&lat=53.8007554&lng=-1.5490773999999874&currentLocation=Leeds&distance=0&distanceUnit=Miles&defaultSearchableLocs=STORE%2C+HOTSPOT%2C+MICROEVENT%2C+BRANDED_STORE&pageNumber="+i;
 
 
 var casper = require('casper').create({
