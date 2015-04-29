fecd19d437dd2ca3984e780f1ce860f33d5ec5c4
Omid Hezaveh <a@gik.fi>
2015-04-29 20:07:31 +0300

    currysUK start work on it

---
+++ casper/currys-uk.js
@@ -0,0 +1,87 @@
+/* 
+ * to scrape Currys uk! 
+ */
+
+
+var i = 0,
+    shopInfo = [],
+    shopsOnPage = [],
+    abortedPrefixes = [];
+
+var startExecutionTime = new Date().getTime();
+
+function onlyUnique(items) { 
+    for (var ix=0; ix<items.length; ix++) {
+    var listI = items[ix];
+    loopJ: for (var jx=0; jx<items.length; jx++) {
+        var listJ = items[jx];
+        if (listI === listJ) continue; //Ignore itself
+        for (var kx=listJ.length; kx>=0; kx--) {
+            if (listJ[kx] !== listI[kx]) continue loopJ;
+        }
+        // At this point, their values are equal.
+        items.splice(jx, 1);
+        }
+    }
+
+    return items;
+}
+
+function saveToFile(finalData, branchName) {
+
+var date = new Date(),
+    minute = date.getMinutes(),
+    day = date.getDate(),
+    hours = date.getHours() +1,
+    month = date.getMonth() + 1, 
+    fs = require('fs');
+
+var fname = branchName+'-'+month+'-'+day+'-'+hours+'-'+minute+'.txt';
+var savePath = fs.pathJoin(fs.workingDirectory,'output',fname);
+    fs.write(savePath, JSON.stringify(finalData), 'w');
+
+}
+
+function logToMainReport(finalData, branchName) {
+
+var date = new Date(),
+    minute = date.getMinutes(),
+    day = date.getDate(),
+    hours = date.getHours() +1,
+    month = date.getMonth() + 1, 
+    year = date.getFullYear();
+    fs = require('fs');
+
+var completionTime = new Date().getTime();
+var executionTime = (completionTime - startExecutionTime)/60000;
+var fname = 'main-report.txt';
+var report = '\n ----------- ' + 
+             'Date: ' +
+             + year +'/'+ month +'/'+ day +'  '+ hours +':'+ minute +
+             ' ----------- \n ' + 
+             'Branch name: ' +
+             branchName + 
+             '\n Number of shops returned: ' +
+             finalData.length +
+             '\n Execution Time (minute): '+
+             executionTime  +  
+             '\n ---\n';
+
+var savePath = fs.pathJoin(fs.workingDirectory,'output',fname);
+    fs.write(savePath, report, 'a');
+
+    casper.echo(report);
+
+}
+var casper = require('casper').create({
+    verbose: false,
+    logLevel: 'info',
+    pageSettings: {
+        loadImages:  false,
+        loadPlugins: false
+        }
+    });
+var baseUrl = 'http://www.currys.co.uk/gbuk/s/find-a-store.html';
+
+
+casper.start(baseUrl);
