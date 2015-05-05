749f541296d7e58b77da2c294facb90a22351cc5
Omid Hezaveh <a@gik.fi>
2015-05-05 12:14:43 +0300

    lloydspharmacyUK add and work right!

---
+++ casper/lloydspharmacy-uk.js
@@ -0,0 +1,97 @@
+/* 
+ * to scrape Lloydes pharmacy uk! 
+ */
+
+
+var i = 0,
+    shopInfo = [],
+    shopsOnPage = [];
+
+
+var startExecutionTime = new Date().getTime();
+
+function onlyUnique(items) { 
+    for (var ix=0; ix<items.length; ix++) {
+    var listI = items[ix];
+    loopJ: for (var jx=0; jx<items.length; jx++) {
+        var listJ = items[jx];
+        if (listI === listJ) continue; //Ignore itself
+        for (var kx=listJ.length; kx>=0; kx--) {
+            if (listJ[kx] !== listI[kx]) continue loopJ;
+        }
+        // At this point, their values are equal.
+        items.splice(jx, 1);
+        }
+    }
+
+    return items;
+}
+
+function saveToFile(finalData, branchName) {
+
+var date = new Date(),
+    minute = ('0'+ date.getMinutes()).slice(-2),
+    day = ('0' + date.getDate()).slice(-2),
+    hours = ('0' + (date.getHours())).slice(-2),
+    month = ('0' + (date.getMonth() + 1)).slice(-2), 
+    year = date.getFullYear(),
+    fs = require('fs');
+
+var fname = branchName+'-'+year+month+day+'-'+ hours + minute+'.txt';
+var savePath = fs.pathJoin(fs.workingDirectory,'output',fname);
+    fs.write(savePath, JSON.stringify(finalData), 'w');
+
+var completionTime = new Date().getTime();
+var executionTime = (completionTime - startExecutionTime)/60000;
+var fLogName = 'main-report.txt';
+var report = '\n ----------- ' + 
+             'Date: ' +
+             + year +'/'+ month +'/'+ day +'  '+ hours +':'+ minute +
+             ' ----------- \n ' + 
+             'Branch name: ' +
+             branchName + 
+             '\n Number of shops returned: ' +
+             finalData.length +
+             '\n Execution Time (minute): '+
+             executionTime  +  
+             '\n Results are saved in: \n'+
+             savePath +
+             '\n ---\n';
+
+var logSavePath = fs.pathJoin(fs.workingDirectory,'output',fLogName);
+    fs.write(logSavePath, report, 'a');
+
+    casper.echo(JSON.stringify(finalData));
+    casper.echo('_-_-_-_-_ Report _-_-_-_-_');
+    casper.echo(report);
+}
+
+var casper = require('casper').create({
+    verbose: true,
+    logLevel: 'debug',
+    pageSettings: {
+        loadImages:  false,
+        loadPlugins: false
+        }
+    });
+
+// prefCoords[i][1] = lat and [i][2] = long
+var baseUrl= 'http://www.lloydspharmacy.com/webapp/wcs/stores/servlet/AjaxStoreLocatorDisplayView?catalogId=11101&langId=44&storeId=10151';
+var baseUrlTest = 'http://google.com';
+
+casper.start(baseUrl);
+casper.wait(14000);
+casper.then(function requestGrabData(){
+    this.capture('screenshots/loyds.png');
+    shopInfo = casper.evaluate(function(){
+
+        return markers.map(function(val){
+              return [+val.position.A, +val.position.F, val.title]
+              });
+        });
+    casper.echo(shopInfo);
+    });
+casper.then(function saveLog(){
+    saveToFile(shopInfo, "lloydspharmacy");
+    });
+casper.run();
