fc954d7c087211a9d4c8abfb6a4635d4e55585be
Omid Hezaveh <a@gik.fi>
2015-05-19 06:54:34 +0200

    pasrser: require defined url

--- casper/parser/routes/index.js
+++ casper/parser/routes/index.js
@@ -1,4 +1,5 @@
-"use strict"
+"use strict";
+
 var express = require("express");
 var router = express.Router();
 var http = require("http");
@@ -11,6 +12,17 @@ router.get("/", function(req, res, next) {
 
 // the fetch page
 router.get("/fetch", function(req, res, next) {
-  res.render("index", { title: "Express" });
+  if(req.query){
+    if(req.query.url === undefined){
+        res.send({messsage: "url cannot be undefined"});
+      }
+      var urlPrefix = req.query.url.match(/.*?:\/\//g);
+      req.query.url = req.query.url.replace(/.*?:\/\//g,"");
+      var options = {
+        hostname: req.query.url
+      };
+      
+      
+    }
 });
 module.exports = router;
