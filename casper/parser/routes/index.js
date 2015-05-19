853265678063f816e4b076cde46557c3a0f44509
Omid Hezaveh <a@gik.fi>
2015-05-19 06:41:56 +0200

    parser: confing Eslint

--- casper/parser/routes/index.js
+++ casper/parser/routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
+"use strict"
+var express = require("express");
 var router = express.Router();
-
+var http = require("http");
+var https = require("https");
+var htmlparser = require("htmlparser2");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
+// the fetch page
+router.get("/fetch", function(req, res, next) {
+  res.render("index", { title: "Express" });
+});
 module.exports = router;
